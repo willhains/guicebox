c0364ff5fe6a8927e671255087e3a5ceb7aece77
willhains <willhains@c64fa8c4-624c-0410-8229-0986fd20f4ef>
2008-05-25 05:56:44 +0000

    Bind constants to BindingAnnotations automatically.

--- trunk/test/org/codeshark/guicebox/TestMain.java
+++ trunk/test/org/codeshark/guicebox/TestMain.java
@@ -10,29 +10,24 @@ public class TestMain extends AbstractModule
 	public static void main(String[] args) throws InterruptedException
 	{
 		// Initialise application
-		GuiceBox.init(Guice.createInjector(
+		final GuiceBox guicebox = GuiceBox.init(Guice.createInjector(
 			new TestMain(),
-			new CommandLineModule(args)
-			    .bind(Param1.class, "param1", "default1")
-			    .bind(Param2.class, "param2", "default2")
-			    .bind(Param3.class, "param3", "default3")
-			    .bind(Param4.class, "param4", "default4")));
+			new CommandLineModule(args)));
 		
 		// Start application
-		GuiceBox.start();
+		guicebox.start();
 		
 		// Wait, then start application again
 		Thread.sleep(10000);
 		System.out.println("------------------------");
-		GuiceBox.start();
+		guicebox.start();
 		
 		// Wait, then kill application
 		Thread.sleep(2000);
-		GuiceBox.kill();
+		guicebox.kill();
 	}
 	
-	@Override
-	public void configure()
+	@Override public void configure()
 	{
 		bind(StartableInterface.class).to(StartableInterfaceImpl.class).in(Scopes.SINGLETON);
 		bind(TestStartable.class);
