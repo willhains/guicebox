1256f225607122abc8046a8cd787dddf46f68f40
willhains <willhains@c64fa8c4-624c-0410-8229-0986fd20f4ef>
2008-06-03 10:04:42 +0000

    Changed the way to initialise GuiceBox so it encapsulates the Injector.

--- trunk/test/org/codeshark/guicebox/TestFailover.java
+++ trunk/test/org/codeshark/guicebox/TestFailover.java
@@ -1,5 +1,7 @@
 package org.codeshark.guicebox;
 
+import static org.codeshark.guicebox.Log.*;
+
 import com.google.inject.*;
 import org.codeshark.guicebox.ha.*;
 
@@ -12,21 +14,23 @@ public class TestFailover extends AbstractModule
 	public static void main(String[] args) throws InterruptedException
 	{
 		// Initialise application
-		final GuiceBox guicebox = GuiceBox.init(Guice
-		    .createInjector(new TestFailover(), new FailoverModule("TestFailover"), new CommandLineModule(args)));
+		final GuiceBox guicebox = GuiceBox.init(
+			new TestFailover(),
+			new FailoverModule("TestFailover"),
+			new CommandLineModule(args));
 		
 		// Start application
-		System.out.println("------STARTING GUICEBOX------");
+		log.info("------STARTING GUICEBOX------");
 		guicebox.start();
 		
 		// Wait, then start application again
 		Thread.sleep(20000);
-		System.out.println("------STARTING GUICEBOX------");
+		log.info("------STARTING GUICEBOX------");
 		guicebox.start();
 		
 		// Wait, then kill application
 		Thread.sleep(20000);
-		System.out.println("------KILLING GUICEBOX------");
+		log.info("------KILLING GUICEBOX------");
 		guicebox.kill();
 	}
 	
